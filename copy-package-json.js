cd6ddabf4e7137455ce8051c822717517f40364b
Szilveszter Safar <szilveszter.safar@gmail.com>
2022-02-07 12:59:17 +0100

    Forgot to copy README to dist

--- copy-package-json.js
+++ copy-package-json.js
@@ -6,4 +6,4 @@ delete package.scripts;
 delete package.devDependencies;
 
 fs.writeFileSync('./dist/package.json', JSON.stringify(package, null, 4));
-
+fs.copyFileSync('./README.md', './dist/README.md');
